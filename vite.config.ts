6ab77ab10b38fbb762309d9882c0d91c020b837b
Creative Minds MP <info@campuae.com>
2025-09-29 16:39:45 +0400

    chore(deploy): configure GitHub Pages and set Vite base for ain-mall

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    // Set base for GitHub Pages project site: https://github.com/hanyfas/ain-mall
+    base: '/ain-mall/',
 })
